4677e8274446a40bf7cd11532102bc1ffcd5f2fb
Jake Mussler <jake.mussler@protonmail.com>
2022-06-08 12:46:02 +0100

    simple REPL implemented.

--- src/main.go
+++ src/main.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Urvirith/bearlang/src/repl"
+)
+
+func main() {
+	fmt.Printf("This is the REPL of BearLang\n")
+	fmt.Printf("Type in a command\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
