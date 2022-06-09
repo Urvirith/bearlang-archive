1b7d477ea2cd652375d3c97e834c0b4210e11586
Jake Mussler <jake.mussler@protonmail.com>
2022-06-09 17:22:19 +0100

    update ast and parser, add Let parsing

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -31,9 +31,9 @@ type Program struct {
 	Statements []Statement
 }
 
-func (prog *Program) TokenLiteral() string {
-	if len(prog.Statements) > 0 {
-		return prog.Statements[0].TokenLiteral()
+func (prg *Program) TokenLiteral() string {
+	if len(prg.Statements) > 0 {
+		return prg.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
--- src/parser/parser.go
+++ src/parser/parser.go
@@ -26,5 +26,64 @@ func (psr *Parser) nextToken() {
 }
 
 func (psr *Parser) ParseProgram() *ast.Program {
-	return nil
+	prg := &ast.Program{}
+	prg.Statements = []ast.Statement{}
+
+	for psr.curToken.Type != token.EOF {
+		stmt := psr.parseStatement()
+		if stmt != nil {
+			prg.Statements = append(prg.Statements, stmt)
+		}
+		psr.nextToken()
+	}
+
+	return prg
+}
+
+func (psr *Parser) parseStatement() ast.Statement {
+	switch psr.curToken.Type {
+	case token.LET:
+		return psr.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (psr *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: psr.curToken}
+
+	// Let is not followed by Identifer (Variable)
+	if !psr.expectPeek(token.ID) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: psr.curToken, Value: psr.curToken.Literal}
+
+	// Identifer is not followed by an Assign
+	if !psr.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO : Skimming expressioins until semicolon
+	for !psr.curTokenIs(token.SCOLON) {
+		psr.nextToken()
+	}
+
+	return stmt
+}
+
+func (psr *Parser) curTokenIs(tok token.TokenType) bool {
+	return psr.curToken.Type == tok
+}
+
+func (psr *Parser) peekTokenIs(tok token.TokenType) bool {
+	return psr.peekToken.Type == tok
+}
+
+func (psr *Parser) expectPeek(tok token.TokenType) bool {
+	if psr.peekTokenIs(tok) {
+		psr.nextToken()
+		return true
+	}
+	return false
 }
